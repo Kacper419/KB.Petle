d26aa86749d0f105ac6571b5183fed9100faab00
jan <jan@gmail.com>
2023-10-12 09:00:56 +0200

    commit nr 6

--- src/zadanie2.java
+++ src/zadanie2.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
 public class zadanie2 {
-}
+    public static void main(String[] args) {
+
+        Scanner klawiatura = new Scanner(System.in);
+
+        int  droga, szybkosc, czas ;
+
+        System.out.println("Podaj szybkość drogi");
+        szybkosc= klawiatura.nextInt();
+
+        System.out.println("czas drogi");
+        czas= klawiatura.nextInt();
+
+        droga=szybkosc*czas;
+
+
+        for (int i = 1;  i<=czas; i++) {
+            droga += 1;
+        }
+
+        system.out.printl("odleglosc przebyta to" + droga + "");
+
+
+    }
