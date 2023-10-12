7213bbbae2b546ef1bd41d8d780c49c09d645bdf
jan <jan@gmail.com>
2023-10-12 08:49:45 +0200

    commit nr 4

--- src/zadanie1.java
+++ src/zadanie1.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class zadanie1 {
+    public static void main(String[] args) {
+
+        Scanner klawiatura = new Scanner(System.in);
+
+        int n;
+        n = klawiatura.nextInt();
+
+        int suma = 0;
+
+        for (int i = 1;  i<=n; i++) {
+            suma += 1;
+        }
+
+     system.out.printl("Suma licz od do" + n + "wynosi" + suma );
+
+
+    }
 }
+
